832883972396f171f9e90eb9621b75f7e0b6f1bd
neutrino658 <32984219+neutrino658@users.noreply.github.com>
2019-11-14 11:46:41 -0800

    delete some

--- plugins/toggle_sidebar/init.js
+++
@@ -1,9 +0,0 @@
-Plugins.Toggle_Sidebar = {
-    toggle: function() {
-        Feeds.toggle();
-
-        const label = document.querySelector("i.toggle-sidebar-label");
-
-        label.innerHTML = Element.visible("feeds-holder") ? 'chevron_left' : 'chevron_right';
-    }
-};
